e915996844437a7c938086fcbb15685e567fba29
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-10-11 16:57:27 +0530

    update

--- index2.js
+++ index2.js
@@ -1,8 +1,8 @@
 function event1(){
     var num1 = (document.getElementById("inputPassword1").value);
     var num2 = (document.getElementById("inputPassword2").value);
-    
-     if(num1===num2){
+
+     if(num1.trim().toLowerCase()===num2.trim().toLowerCase()){
         document.getElementById("result1").innerText =  "Equal";
      }
         else{
@@ -12,7 +12,7 @@ function event2(){
     var num1 = (document.getElementById("inputPassword3").value);
     var num2 =(document.getElementById("inputPassword4").value);
 
-    if(num1===num2){
+    if(num1.trim().toLowerCase()===num2.trim().toLowerCase()){
         document.getElementById("result2").innerText =  "Equal";
     }
     else{
