3c21163f1295e99a4725c1295f818956b47c971a
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-10-19 09:55:01 +0530

    update

--- index3.js
+++ index3.js
@@ -1,6 +1,6 @@
 function setCapital(){
-    const text3=document.getElementById("inputPassword1").value;
-    
+    const text3=(document.getElementById("inputPassword1").value);
+
     if(text3.length>0){
         const capitalizedFirstletter=text3.charAt(0).toUpperCase()+text3.slice(1).toLowerCase();
         document.getElementById("result3").innerText=capitalizedFirstletter;
