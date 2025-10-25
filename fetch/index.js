85451ba52cd27eafaa4938337d47af35319bf225
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-10-25 11:45:20 +0530

    update

--- fetch/index.js
+++ fetch/index.js
@@ -1,25 +1,28 @@
-function dataGet() {
+    function saveData() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+        .then((response) => response.json())
+        .then((json) => {
 
+        const cards = json.map((post) => {
+                return `
+                    <div style="
+                        display: inline-block;
+                        margin: 10px;
+                        border: 2px solid black;
+                        border-radius: 10px;
+                        padding: 10px;
+                        width: 250px;
+                        text-align: center;
+                        box-shadow: 2px 2px 5px rgba(0,0,0,0.2);
+                    ">
+                        <h3>${post.title}</h3>
+                        <p>${post.body}</p>
+                        <p>${post.id} </p>
+                    </div>
+                `;
+            });
+
+            document.getElementById("result").innerHTML = cards.join('');
+        })
+        
 }
-dataGet();
-
-function saveData(){
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-
-}
-saveData();
